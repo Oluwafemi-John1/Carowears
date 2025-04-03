1af370efc816c6dcb0db40e76ceb72835e811b32
Oyeniran Oluwafemi John <oyeniranoluwafemi36@gmail.com>
2025-04-03 09:44:55 +0100

    update API endpoint to use Vercel server URL

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,7 +2,8 @@ import './App.css'
 import axios from 'axios'
 
 function App() {
-    const url = 'http://localhost:5400/test'
+    // const url = 'http://localhost:5400/test'
+    const url = 'https://carowears-server.vercel.app/test'
     const getInfo = () => {
         axios.get(url)
         .then((res)=>{
